783f1869be990f9c0a2faa96972e3e4363cf43b7
Gabriel Enslein <genslein16@gmail.com>
2021-04-18 15:32:39 -0400

    add testing for version compare for iterator through versions

--- exercises/npm-registry/src/package.ts
+++ exercises/npm-registry/src/package.ts
@@ -9,9 +9,10 @@ export const getPackage: RequestHandler = async function (req, res, next) {
   const { name, version } = req.params;
 
   try {
-    const dependencies = queryNpmSource(name)[version].dependencies;
+    const dependencies = (await queryNpmSource(name)).versions[version].dependencies;
 
-    return res.status(200).json({ name, version, dependencies });
+    const results = constructDependencyTree(dependencies);
+    return res.status(200).json({ name, version, results });
   } catch (error) {
     return next(error);
   }
@@ -22,16 +23,24 @@ export const queryNpmSource = async function (name) {
     `https://registry.npmjs.org/${name}`,
   ).json();
 
-  return npmPackage.versions;
+  return npmPackage;
 }
 
+// Uses recursion to build the tree
+// TODO: While dependency resolution should be a tree
+//       it is theoretically possible to have cycle creating a graph instead. Cycles
+//       would hang the current algorithm and would need additional checks
+//       to validate continuing tree construction.
+//       Implementing a "seen" cache of subtrees would allow early termination in these cases.
 export const constructDependencyTree = function (dependencies) {
   let possibleVersions;
   let target;
+  let npmPackage;
 
   for (let [key, value] of Object.entries(dependencies)) {
-    possibleVersions = queryNpmSource(key);
-    target = evaluateRule(value, possibleVersions);
+    npmPackage = queryNpmSource(key);
+    possibleVersions = npmPackage.versions;
+    target = evaluateRule(value, npmPackage);
 
     dependencies[key] = {
       "rule": key,
@@ -54,14 +63,31 @@ export const constructDependencyTree = function (dependencies) {
 // Difficulty in non-standard version evaluation
 // react-is Example: "16.4.0-alpha.3174632",
 //                   "16.4.0-alpha.0911da3"
-export const evaluateRule = function (rule, versions) {
+export const evaluateRule = function (rule, npmPackage) {
+  const regex = /[0-9]/g;
   let target;
+  let versions = npmPackage.versions
+
+  let evaluator;
+  let major = "";
+  let minor = "";
+  let patch = "";
+  let operand = "";
+
+  // Removes some cases from logic
+  if (rule == "" || rule == "*" || rule == "x") {
+    return npmPackage["dist-tags"]["latest"];
+  }
 
   // js-tokens allows multiple rules chained: "^3.0.0 || ^4.0.0"
+  // trim whitespace for each
+  // assumes the highest allowed in the OR based on actual npm behavior
   if (rule.includes("||")) {
-    let passOne = rule.split("||")[0];
-    let passTwo = rule.split("||")[1];
+    let firstOrArg = rule.split("||")[0].trim();
+    let secondOrArg = rule.split("||")[1].trim();
+    firstOrArg[0].match(regex)
   }
+
   // determine major, minor, patch
   if (rule[0] == '~') {
 
@@ -84,11 +110,50 @@ export const evaluateRule = function (rule, versions) {
 
   }
 
-  for (let [key, value] of Object.entries(versions)) {
- 
-  }
+  // for (let key in versions) {
+
+  // }
 
   return target;
+}
+
+// assumes actual versions major, minor, patch are numerics only
+export const compareVersions = function (possible, currentHighest) {
+  const versionRegex = /[\s.-]+/;
+  const preReleaseLookup = { "alpha": 1, "beta": 2, "rc": 3};
+
+  let possibleParts = possible.split(versionRegex);
+  let currentHighestParts = currentHighest.split(versionRegex);
+
+  // major
+  if (parseInt(possibleParts[0]) > parseInt(currentHighestParts[0])) {
+    return possible;
+  }
+
+  // minor
+  if (parseInt(possibleParts[1]) > parseInt(currentHighestParts[1])) {
+    return possible;
+  }
+
+  // patch
+  if (parseInt(possibleParts[2]) > parseInt(currentHighestParts[2])) {
+    return possible;
+  }
+
+  // if we need to comparate pre-release versions
+  if (possibleParts.length > 2) {
+    if (currentHighestParts.length > 2 &&
+      preReleaseLookup[possibleParts[3]] > preReleaseLookup[currentHighestParts[3]]) {
+       return possible;
+    } // else return currentHighest because pre-release is lower than official
+  }
+
+  // current highest is a pre-release while new possible is not
+  if (currentHighestParts.length > 2) {
+    return possible;
+  }
+
+  return currentHighest;
 }
 
 
--- exercises/npm-registry/test/package.test.ts
+++ exercises/npm-registry/test/package.test.ts
@@ -2,43 +2,81 @@ import * as getPort from 'get-port';
 import got from 'got';
 import { Server } from 'http';
 import { createApp } from '../src/app';
+import { evaluateRule, compareVersions } from '../src/package';
 
-describe('/package/:name/:version endpoint', () => {
-  let server: Server;
-  let port: number;
+// describe('/package/:name/:version endpoint', () => {
+//   let server: Server;
+//   let port: number;
 
-  beforeAll(async (done) => {
-    port = await getPort();
-    server = createApp().listen(port, done);
+//   beforeAll(async (done) => {
+//     port = await getPort();
+//     server = createApp().listen(port, done);
+//   });
+
+//   afterAll((done) => {
+//     server.close(done);
+//   });
+
+//   it('responds', async () => {
+//     const packageName = 'react';
+//     const packageVersion = '16.13.0';
+
+//     const res: any = await got(
+//       `http://localhost:${port}/package/${packageName}/${packageVersion}`,
+//     ).json();
+
+//     expect(res.name).toEqual(packageName);
+//   });
+
+//   it('returns dependencies', async () => {
+//     const packageName = 'react';
+//     const packageVersion = '16.13.0';
+
+//     const res: any = await got(
+//       `http://localhost:${port}/package/${packageName}/${packageVersion}`,
+//     ).json();
+
+//     expect(res.dependencies).toEqual({
+//       'loose-envify': '^1.1.0',
+//       'object-assign': '^4.1.1',
+//       'prop-types': '^15.6.2',
+//     });
+//   });
+// });
+
+describe ('compareVersions function', () => {
+  it('returns the release version instead of pre-release',  () => {
+    let preRelease = "16.4.0-alpha.3174632";
+    let release = "16.4.0";
+
+    expect(compareVersions(release, preRelease)).toEqual(release);
   });
 
-  afterAll((done) => {
-    server.close(done);
+  it('returns the higher prerelease version',  () => {
+    let preRelease = "16.4.0-alpha.3174632";
+    let release = "16.4.0-rc.0";
+
+    expect(compareVersions(release, preRelease)).toEqual(release);
   });
 
-  it('responds', async () => {
-    const packageName = 'react';
-    const packageVersion = '16.13.0';
+  it('returns the higher patch version',  () => {
+    let preRelease = "16.4.0-alpha.3174632";
+    let release = "16.4.1-alpha.0";
 
-    const res: any = await got(
-      `http://localhost:${port}/package/${packageName}/${packageVersion}`,
-    ).json();
-
-    expect(res.name).toEqual(packageName);
+    expect(compareVersions(release, preRelease)).toEqual(release);
   });
 
-  it('returns dependencies', async () => {
-    const packageName = 'react';
-    const packageVersion = '16.13.0';
+  it('returns the higher minor version',  () => {
+    let preRelease = "16.4.0-alpha.3174632";
+    let release = "16.5.0-alpha.0";
 
-    const res: any = await got(
-      `http://localhost:${port}/package/${packageName}/${packageVersion}`,
-    ).json();
+    expect(compareVersions(release, preRelease)).toEqual(release);
+  });
 
-    expect(res.dependencies).toEqual({
-      'loose-envify': '^1.1.0',
-      'object-assign': '^4.1.1',
-      'prop-types': '^15.6.2',
-    });
+  it('returns the higher major version',  () => {
+    let preRelease = "16.4.0-alpha.3174632";
+    let release = "17.4.0-alpha.0";
+
+    expect(compareVersions(release, preRelease)).toEqual(release);
   });
 });
